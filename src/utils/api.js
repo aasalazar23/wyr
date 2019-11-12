8a2d4a5c74a09940c8d0503ecc055df0b8f1f319
Alejandro <aasalazar23@gmail.com>
2019-11-12 08:04:55 -0700

    Initial DB setup

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer
+} from "./_DATA";
+
+export function saveQuestion(info) {
+  return _saveQuestion(info)
+}
+
+export function saveQuestionAnswer(info) {
+  return _saveQuestionAnswer(info)
+}
